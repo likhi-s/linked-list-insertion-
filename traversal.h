ec33aa28e938d549b30cb09c716013476da0e6bd
Likitha <likithasshivareddy@gmail.com>
2024-12-17 15:43:40 +0530

    traversal

---
+++ traversal.h
@@ -0,0 +1,14 @@
+#ifndef TRAVERSAL_H
+#define TRAVERSAL_H
+
+
+enum traversal
+{
+    FORWARD_TRAVERSAL=3,NODE_COUNT,EXIT_FROM_TRAVERSAL
+};
+
+void traversal();
+void forwardTraversal();
+void nodeCount();
+
+#endif // TRAVERSAL_H
